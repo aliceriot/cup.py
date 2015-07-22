df9c5d24184f716f2f130a73ba8a6e4c46fbf499
Alice Pote <alice.writes.wrongs@gmail.com>
2015-07-22 17:41:15 -0400

    Trying to get the rectangle right

--- editor.py
+++ editor.py
@@ -19,14 +19,15 @@ class Editor():
         """
         puts a list of open buffers at the top
         """
-        rectangle(self.screen, 0,0, 1, curses.COLS -1)
+        rectangle(self.screen, 0,0, 2, curses.COLS -1)
         self.screen.refresh()
-        buflist = curses.newwin(1, curses.COLS-1, 0,0)
+        buflist = curses.newwin(1, curses.COLS-1, 1,0)
         buflist.refresh()
+        bufferlist = []
         if (active == ''):
             bufferlist = ["new file"]
         bufferlist += list(self.buffers.keys())
-        buflist.addstr(" | ".join(bufferlist))
+        buflist.addstr("buffers:" + " | ".join(bufferlist))
         buflist.refresh()
 
     def add_buffer(self, filename, text):
@@ -50,8 +51,9 @@ class Buffer():
         self.text = text
 
     def edit_buffer(self):
-        screen = curses.newwin(curses.LINES-1, curses.COLS-1, 1,0)
+        screen = curses.newwin(curses.LINES-1, curses.COLS-1, 3,0)
         screen.addstr(self.text)
         textbox = Textbox(screen)
+        textbox.stripspaces = False
         textbox.edit()
         self.text = textbox.gather()
