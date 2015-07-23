90cdef8be3dd2ac0d37e0cd86d6bb06d1ba4c116
Alice Pote <alice.writes.wrongs@gmail.com>
2015-07-23 15:31:25 -0400

    Basically everything happens in the Editor object now

--- cup.py
+++ cup.py
@@ -3,21 +3,7 @@ import curses
 import sys
 import os
 
-### initialization, create editor object and initialize buffers
+### create editor object, initialize with files to open, listen for keypresses
 cup = Editor()
-
-# this should be a method too...
-files_to_open = sys.argv[1:]
-for to_open in files_to_open:
-    if (os.path.isfile(to_open)):
-        with open(to_open) as myfile:
-            cup.add_buffer(to_open, myfile.read())
-    else:
-        cup.add_buffer(to_open, '')
-    if cup.current_buffer == '':
-        cup.current_buffer = to_open
-cup.buffer_list(cup.current_buffer)
-cup.switch_buffer(cup.current_buffer)
-
-# the listen method handles the rest!
+cup.initialize(sys.argv[1:])
 cup.listen()
